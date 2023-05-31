404768bc578bdf7191ad6498b5a79d4fdd9fc76f
3121kz <zelinsky.erik17@gmail.com>
2023-05-31 08:36:15 +0200

    Change to home and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,14 @@ function Navbar() {
         <img src={Logo} alt="Bloodborne" />
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
+          <Link to="/items"> Items </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
       </div>
       <div className="right">
         <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
+        <Link to="/items"> Items </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
         <button onClick={toggleNavbar}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Helo Svetu</div>;
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>Bloodborne</h1>
+        <p> The hunt begins </p>
+        <Link to="/">
+          <button>LEARN MORE!</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
