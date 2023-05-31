d53bdab1633506fe1bbca5db14b1d6b2e1d2925d
3121kz <zelinsky.erik17@gmail.com>
2023-05-31 08:47:27 +0200

    Home page + style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Bg from "../images/bloodborneBg.jpg";
+import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="home">
-      <div className="header">
+      <div className="header" style={{ backgroundImage: `url(${Bg})` }}>
         <h1>Bloodborne</h1>
         <p> The hunt begins </p>
         <Link to="/">
