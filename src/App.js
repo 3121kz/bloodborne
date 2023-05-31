53dee94a71dbd2a68911d20baeb0a24fe31681d2
3121kz <zelinsky.erik17@gmail.com>
2023-05-31 07:04:36 +0200

    Made Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Helo</div>;
+}
+
+export default Home;
